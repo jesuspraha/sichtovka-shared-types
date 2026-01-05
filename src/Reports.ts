9ed8bca2345af6a132ab7e5cbb56ec7d394ac05d
jesuspraha <jesus@orechovka.cz>
2026-01-05 15:29:54 +0100

    feat: add EmployeeReportItem and ZakazkaReportItem types for reporting

--- src/Reports.ts
+++ src/Reports.ts
@@ -3,6 +3,7 @@ export type EmployeeReportItem = {
     user_id: number;
     jmeno: string;
     hodin: number;
+    km: number;
     procento: number;
     mzda: number;
     odmena: number;
@@ -20,8 +21,10 @@ export type ZakazkaReportItem = {
     km: number;
     naklady_zakazka: number;
     naklady_mzdy: number;
+    naklady_doprava: number;
     naklady_celkem: number;
     fakturace: number;
+    ziskmat: number;
     zisk: number;
     zamestnanci: EmployeeReportItem[];
 };
